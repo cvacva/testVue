8c870fbde6cb1006725ea581de534f05f091a362
cvacva <769978880@qq.com>
2020-05-29 15:22:15 +0800

    上传代码的注释

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        we
+        <div id="main" style="width: 600px;height:400px;"></div>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {}
+        },
+        methods: {
+            initChart() {
+                console.log('22')
+                // var myChart = echarts.init(document.getElementById('main'));
+            }
+        },
+        mounted() {
+            this.initChart()
+        }
+    }
+</script>
